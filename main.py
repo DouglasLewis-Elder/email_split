ba77de1c872314b9f099bb0177de7ad17f481380
21086 <21086@wc.school.nz>
2025-02-18 11:50:36 +1300

    Fixed spelling u little kartik

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 email = input("Enter Email here:")
 email = email.split('@')
-print("You're Username is", email[0], "Your domain is", email[1])
+print("Your Username is", email[0], "Your domain is",email[1])
